7487d4946d45e633d29422100bb8e06bfb72a7c7
Josh Inman <joshua.inman@okstate.edu>
2022-04-27 10:22:58 -0500

    Added state size to data model/source

--- app/src/main/java/com/example/mad_ind05_inman_joshua/State.kt
+++ app/src/main/java/com/example/mad_ind05_inman_joshua/State.kt
@@ -1,3 +1,3 @@
 package com.example.mad_ind05_inman_joshua
 
-data class State(val name: String, val nickname: String)
+data class State(val name: String, val nickname: String, val size: String)
---
+++ app/src/main/java/com/example/mad_ind05_inman_joshua/StateDetails.kt
@@ -0,0 +1,11 @@
+package com.example.mad_ind05_inman_joshua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StateDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_details)
+    }
+}
